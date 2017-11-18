000d6c695edb1f017d3efa8a4861b59b77d12b01
RavicharanN <ravicharan.vsp@gmail.com>
2017-11-18 17:38:44 +0530

    Add alarm feature

--- src/javaapplication9/NewJFrame.java
+++ src/javaapplication9/NewJFrame.java
@@ -17,10 +17,17 @@ public class NewJFrame extends javax.swing.JFrame {
     /**
      * Creates new form NewJFrame
      */
+    boolean isSelected = false;
+    boolean alarmIsSet = false;
+    int alarm_hrs;
+    int alarm_min;
+    int alarm_sec;
+    String amPm;
+    
     public NewJFrame() {
         initComponents(); 
         choice1.add("America/Los_Angeles");
-        choice1.add("America/Los_Angeles");
+        //choice1.add("America/Los_Angeles");
         choice1.add("Asia/Colombo");
         choice1.add("Canada/Central");
         choice1.add("Egypt");
@@ -28,6 +35,7 @@ public class NewJFrame extends javax.swing.JFrame {
         choice1.add("Greenwich");
         choice1.add("IST");
         choice1.add("Japan");
+        
         
         System.out.println("Check - 1");
         new Thread(){
@@ -43,11 +51,13 @@ public class NewJFrame extends javax.swing.JFrame {
                    String time="";
                   //System.out.println(time);
                    if(jRadioButton1.isSelected()) {
-                       
+                       isSelected = true;
                        if(AM_PM == 1)
                            hour = hour+12;                               
                        if(hour<=9) 
-                           time = hour+":";                       
+                           time = "0"+hour+":";  
+                       else 
+                           time = hour+":";
                        if(min<=9)
                            time = time+"0"+min+":";
                        else 
@@ -59,6 +69,7 @@ public class NewJFrame extends javax.swing.JFrame {
                        clock.setText(time);
                    }                      
                    else {
+                       isSelected = false;
                        if(hour<=9) 
                            time = "0"+hour+":";
                        else 
@@ -76,14 +87,166 @@ public class NewJFrame extends javax.swing.JFrame {
                        else 
                            time = time + "A.M";
                        clock.setText(time);
-                   }
-                        
+                   }         
                 }    
             }
         }.start();
     }
+    
+    public NewJFrame(boolean tempIsSelected,int tempHrs,int tempMin,int tempSec,String tempAmPm) {
+        alarmIsSet = true;
+        isSelected = tempIsSelected;
+        alarm_hrs = tempHrs;
+        alarm_min = tempMin;
+        alarm_sec = tempSec;
+        amPm = tempAmPm;
+        
+        int alarm_hrs24,alarm_min24,alarm_sec24;
+        int alarm_hrs12,alarm_min12,alarm_sec12;
+        
+        if(amPm.compareTo("") == 0) {
+            // 24 hr format
+           alarm_hrs24 = alarm_hrs;
+           alarm_min24 = alarm_min;
+           alarm_sec24 = alarm_sec;
+           alarm_min12 = alarm_min;
+           alarm_sec12 = alarm_sec;
+           if(alarm_hrs24 >= 12) {
+               amPm = "P.M";
+               if(alarm_hrs24 == 12) 
+                   alarm_hrs12 = 12;
+               else
+                   alarm_hrs12 = alarm_hrs24 - 12;                   
+           }
+           else {
+               amPm = "AM";
+               if(alarm_hrs24 == 0) 
+                   alarm_hrs12 = 12;
+               else 
+                   alarm_hrs12 = alarm_hrs24;
+           }       
+        }
+        else {
+            alarm_hrs12 = alarm_hrs;
+            alarm_min12 = alarm_min;
+            alarm_sec12 = alarm_sec;
+            alarm_min24 = alarm_min;
+            alarm_sec24 = alarm_sec;
+            if(amPm.compareTo("A.M") == 0) {
+                alarm_hrs24 = alarm_hrs12;
+                if(alarm_hrs12 == 12) 
+                    alarm_hrs24 = 0;
+            }
+            else {
+                alarm_hrs24 = alarm_hrs12 + 12;
+                if(alarm_hrs12 == 12) 
+                    alarm_hrs24 = 12;
+            }
+                
+        }
+        
+        System.out.println("alarm set for 24hrs at " +alarm_hrs24+" "+alarm_min24+" "+alarm_sec24);
+        System.out.println("alarm set for 12hrs at " +alarm_hrs12+" "+alarm_min12+" "+alarm_sec12 +""+amPm);
+        
+        initComponents(); 
+        choice1.add("America/Los_Angeles");
+        //choice1.add("America/Los_Angeles");
+        choice1.add("Asia/Colombo");
+        choice1.add("Canada/Central");
+        choice1.add("Egypt");
+        choice1.add("Europe/Berlin");
+        choice1.add("Greenwich");
+        choice1.add("IST");
+        choice1.add("Japan");
+        
+        System.out.println(isSelected);
+        if(isSelected)
+            jRadioButton1.setSelected(true);
+            
+        // Thread 1 
+        new Thread(){
+            public void run() {
+                //clock.setText("23:22:22 A.M");
+                while(true) {                   
+                   Calendar cal = new GregorianCalendar(TimeZone.getTimeZone(choice1.getItem(choice1.getSelectedIndex())));
+                   int hour = cal.get(Calendar.HOUR);
+                   int min = cal.get(Calendar.MINUTE);
+                   int sec = cal.get(Calendar.SECOND);
+                   int AM_PM = cal.get(Calendar.AM_PM);
+                   String time="";
+                  //System.out.println(time);
+                   if(jRadioButton1.isSelected()) {
+                       isSelected = true;
+                       if(AM_PM == 1 && hour > 12)
+                           hour = hour+12;                               
+                       if(hour<=9) 
+                           time = "0"+hour+":";  
+                       else 
+                           time = hour+":";
+                       if(min<=9)
+                           time = time+"0"+min+":";
+                       else 
+                           time = time+min+":";
+                       if(sec<=9)
+                           time = time+"0"+sec+" ";
+                       else
+                           time = time+sec+" ";
+                       clock.setText(time);
+                   }                      
+                   else {
+                       isSelected = false;
+                       if(hour<=9) 
+                           time = "0"+hour+":";
+                       else 
+                           time = hour+":";
+                       if(min<=9)
+                           time = time+"0"+min+":";
+                       else 
+                           time = time+min+":";
+                       if(sec<=9)
+                           time = time+"0"+sec+" ";
+                       else
+                           time = time+sec+" ";
+                       if(AM_PM == 1) 
+                           time = time+"P.M";
+                       else 
+                           time = time + "A.M";
+                       clock.setText(time);
+                   }         
+                }    
+            }
+        }.start();
+        
+        // Thread2 Alarm Thread 
+        new Thread() {
+            public void run() {
+                while(true) {
+                    
+                    Calendar cal = new GregorianCalendar(TimeZone.getTimeZone(choice1.getItem(choice1.getSelectedIndex())));
+                    int hour = cal.get(Calendar.HOUR);
+                    int min = cal.get(Calendar.MINUTE);
+                    int sec = cal.get(Calendar.SECOND);
+                    int AM_PM = cal.get(Calendar.AM_PM);
+                    
+                    if(alarmIsSet) {
+                        if(isSelected) {
+                            
+                        }
+                        else {
+                            
+                        }
+                        
+                    }
+                }          
+            }
+        }.start();
+        
+        
+    }
 
     /**
+     * 
+     * 
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
      * regenerated by the Form Editor.
@@ -96,6 +259,7 @@ public class NewJFrame extends javax.swing.JFrame {
         jRadioButton1 = new javax.swing.JRadioButton();
         choice1 = new java.awt.Choice();
         clock = new javax.swing.JLabel();
+        setAlarm = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -110,8 +274,16 @@ public class NewJFrame extends javax.swing.JFrame {
 
         clock.setFont(new java.awt.Font("Lucida Grande", 0, 24)); // NOI18N
         clock.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        clock.setText("wd");
         clock.setToolTipText("");
         clock.setAutoscrolls(true);
+
+        setAlarm.setText("Set Alarm");
+        setAlarm.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                setAlarmActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -122,14 +294,17 @@ public class NewJFrame extends javax.swing.JFrame {
                     .addGroup(layout.createSequentialGroup()
                         .addGap(98, 98, 98)
                         .addComponent(jLabel1)
-                        .addGap(30, 30, 30)
+                        .addGap(40, 40, 40)
                         .addComponent(clock, javax.swing.GroupLayout.PREFERRED_SIZE, 241, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(173, 173, 173)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(choice1, javax.swing.GroupLayout.PREFERRED_SIZE, 149, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jRadioButton1))))
-                .addContainerGap(151, Short.MAX_VALUE))
+                            .addComponent(jRadioButton1)))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(206, 206, 206)
+                        .addComponent(setAlarm)))
+                .addContainerGap(141, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -145,7 +320,9 @@ public class NewJFrame extends javax.swing.JFrame {
                     .addGroup(layout.createSequentialGroup()
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(clock, javax.swing.GroupLayout.PREFERRED_SIZE, 64, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(114, Short.MAX_VALUE))
+                .addGap(18, 18, 18)
+                .addComponent(setAlarm)
+                .addContainerGap(64, Short.MAX_VALUE))
         );
 
         pack();
@@ -154,6 +331,13 @@ public class NewJFrame extends javax.swing.JFrame {
     private void jRadioButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jRadioButton1ActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_jRadioButton1ActionPerformed
+
+    private void setAlarmActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_setAlarmActionPerformed
+        // TODO add your handling code here:
+        AlarmFrame alarm = new AlarmFrame(isSelected);
+        alarm.setVisible(true);
+        this.setVisible(false);
+    }//GEN-LAST:event_setAlarmActionPerformed
 
     /**
      * @param args the command line arguments
@@ -195,5 +379,6 @@ public class NewJFrame extends javax.swing.JFrame {
     private javax.swing.JLabel clock;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JRadioButton jRadioButton1;
+    private javax.swing.JButton setAlarm;
     // End of variables declaration//GEN-END:variables
 }
